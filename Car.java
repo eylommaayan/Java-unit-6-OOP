009a188c90628a0906e724f32d16795bd67ccd2a
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-04-09 14:27:26 +0300

    // דוגמא לשימוש בבאני

---
+++ Car.java
@@ -0,0 +1,18 @@
+// דוגמא לשימוש בבאני 
+
+public class Car {
+    String make;
+    double price;
+    int year;
+    String color;
+
+    public Car(String make,
+     double price, 
+     int Year,
+      String color){
+        this.make = make;
+        this.price = price;
+        this.year = year;
+        this.color = color;
+    }
+}
