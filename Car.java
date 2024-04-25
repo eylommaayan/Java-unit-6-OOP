27c4410e30de4dba27e884fec0e73341047856f2
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-04-25 22:24:47 +0300

    //עם המשתנים של הבאני this קישור המשתנים הנוחכים

--- Car.java
+++ Car.java
@@ -6,13 +6,14 @@ public class Car {
     int year;
     String color;
 
+    //עם המשתנים של הבאני this קישור המשתנים הנוחכים
     public Car(String make,
      double price, 
      int Year,
       String color){
         this.make = make;
         this.price = price;
-        this.year = year;
+        this.year = Year;
         this.color = color;
     }
 }
