6115c57895b490ee8bad1a76f226ac2c2bc0d364
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-05-08 02:52:23 +0300

    Create p1_person.java

---
+++ p1_person.java
@@ -0,0 +1,10 @@
+public class p1_person {
+    String name;
+    String nationality;
+    String dateOfBirth;
+    String[] passport;
+    int seatNumber; 
+
+    public p1_person(String name, String nationality, String dateOfBirth, int seatNumber){
+    
+}
