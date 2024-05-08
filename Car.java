f4d761081816e3e0e74db1f2e42f58ba640dca62
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-05-08 03:23:06 +0300

    עדכון getters

--- Car.java
+++ Car.java
@@ -1,10 +1,10 @@
 // דוגמא לשימוש בבאני 
 
 public class Car {
-    String make;
-    double price;
-    int year;
-    String color;
+    private  String make;
+    private double price;
+    private int year;
+    private String color;
 
     //עם המשתנים של הבאני this קישור המשתנים הנוחכים
     public Car(String make,
@@ -15,5 +15,23 @@ public class Car {
         this.price = price;
         this.year = Year;
         this.color = color;
+    }
+
+    // getters -  בשביל להשתמש במשתנים הפרטיים 
+
+    public  String getMake(){
+        return make;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public  int getYear(){
+        return  year;
+    }
+
+    public String getColor(){
+        return  color;
     }
 }
