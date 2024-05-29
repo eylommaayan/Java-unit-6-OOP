15141b7042402cde01e6907e3502b03b58f80c66
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-05-29 21:14:33 +0300

    עדכון

--- Car.java
+++ Car.java
@@ -1,24 +1,31 @@
-// דוגמא לשימוש בבאני 
+import java.util.Arrays;
 
 public class Car {
-    private  String make;
-    private double price;
-    private int year;
-    private String color;
+    private String make;   // סוג הרכב
+    private double price;  // מחיר הרכב
+    private int year;      // שנת ייצור הרכב
+    private String color;  // צבע הרכב
+    private String[] spareParts; // חלקי חילוף
 
-    //עם המשתנים של הבאני this קישור המשתנים הנוחכים
-    public Car(String make,
-     double price, 
-     int Year,
-      String color){
+    // קונסטרקטור ראשי לאתחול כל המשתנים
+    public Car(String make, double price, int year, String color, String[] spareParts) {
         this.make = make;
         this.price = price;
-        this.year = Year;
+        this.year = year;
         this.color = color;
+        this.spareParts = Arrays.copyOf(spareParts, spareParts.length); // יצירת עותק כדי לשמור על אי-שינוי חיצוני
     }
 
+    // קונסטרקטור מעתיק לאתחול עותק של אובייקט Car קיים
+    public Car(Car car) {
+        this.make = car.make;
+        this.price = car.price;
+        this.year = car.year;
+        this.color = car.color;
+        this.spareParts = Arrays.copyOf(car.spareParts, car.spareParts.length); // יצירת עותק כדי לשמור על אי-שינוי חיצוני
+    }
 
-
+    // גטרים וסטרים עבור כל המאפיינים
     public String getMake() {
         return this.make;
     }
@@ -50,3 +57,29 @@ public class Car {
     public void setColor(String color) {
         this.color = color;
     }
+
+    public String[] getSpareParts() {
+        return Arrays.copyOf(this.spareParts, this.spareParts.length); // החזרת עותק כדי לשמור על אי-שינוי חיצוני
+    }
+
+    public void setSpareParts(String[] spareParts) {
+        this.spareParts = Arrays.copyOf(spareParts, spareParts.length); // יצירת עותק כדי לשמור על אי-שינוי חיצוני
+    }
+
+    // פונקציה להדפסת פרטי הרכב
+    public void display() {
+        System.out.println("\nCar Details:");
+        System.out.println("Make: " + this.make);
+        System.out.println("Price: " + this.price);
+        System.out.println("Year: " + this.year);
+        System.out.println("Color: " + this.color);
+        System.out.println("Spare Parts: " + String.join(", ", this.spareParts));
+    }
+
+    // פונקציה להדפסת הודעת נהיגה
+    public void drive() {
+        System.out.println("\nYou bought the beautiful " + this.year + " " + this.color + " " + this.make + " for " + this.price + ".");
+        System.out.println("It comes with the following spare parts: " + String.join(", ", this.spareParts));
+        System.out.println("Please drive your car to the nearest exit.\n");
+    }
+}
--- Main.java
+++ Main.java
@@ -1,16 +1,22 @@
-/**
- * Mian
- */
 public class Main {
 
     public static void main(String[] args) {
-        Car nissan = new Car("Nissan", 10000, 2020, "Green");
-        Car dodge = new Car("Dodge", 11000, 2019, "Blue");
+        String[] spareParts = new String[] {"Tires", "Keys"};
 
+        // יצירת אובייקט Car עם פרטי רכב מסוג Nissan
+        Car nissan = new Car("Nissan", 10000, 2020, "Green", spareParts);
 
-        System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() + 
-        ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
-        System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() + 
-        ". It was built in " + dodge.getYear() + ". It is " + dodge.getClass() + ".\n");
+        // יצירת אובייקט Car עם פרטי רכב מסוג Dodge
+        Car dodge = new Car("Dodge", 11000, 2019, "Blue", spareParts);
+
+        // הדפסת פרטי הרכב Dodge
+        dodge.display();
+
+        // יצירת אובייקט חדש על בסיס אובייקט קיים של Nissan ושינוי הצבע שלו
+        Car nissan2 = new Car(nissan);
+        nissan2.setColor("Yellow");
+
+        // קריאה לפונקציה drive להדפסת הודעת נהיגה
+        nissan2.drive();
     }
 }
